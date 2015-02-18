0027327214ba0a5405dc0a443e63f0d07557d055
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-18 21:50:12 -0200

    initial functionality

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  switch(typeof obj){
+  	case "number":
+  		obj = obj.toString();
+  	break;
+  	case "string":
+  		obj;
+  	break;
+  	case "boolean":
+  		obj = obj.toString();
+  	break;
+  	case "object":
+  		var temp ="";
+  		if(Array.isArray(obj)){
+  			temp = "[";
+  			for(var i = 0; i < obj.length; i++){
+  				if(i === obj.length - 1){
+  					temp += stringifyJSON(obj[i]);
+  				} else {
+  					temp += stringifyJSON(obj[i]) + ",";
+  				}
+  			}
+  			temp += "]";	
+  		} else {
+  			obj = "{";
+  			for (var item in obj){
+
+  			}
+  		}
+  		obj = temp;
+  	break;
+  	default:
+  		obj = null;
+  }
+  return obj;
 };
