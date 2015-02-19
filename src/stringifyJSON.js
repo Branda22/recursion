22a07e6f42ba0264d60db2e9c66bfdd78570a0b0
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-19 17:24:33 -0200

    implemented stringifyJSON all tests pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,25 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) { return 'null'; }
+
   switch(typeof obj){
   	case "number":
   		obj = obj.toString();
-  	break;
+  		break;
   	case "string":
-  		obj = obj ? obj : null;
-  	break;
+  		obj = obj ? '"' + obj + '"' : null;
+  		break;
   	case "boolean":
   		obj = obj.toString();
-  	break;
+  		break;
+  	case 'undefined':
+  		return undefined;
+  		break;
   	case "object":
+  		if(obj === null && typeof obj === "object"){
+  			obj = 'null';
+  		} 
   		var temp ="";
   		if(Array.isArray(obj)){
   			temp = "[";
   			for(var i = 0; i < obj.length; i++){
   				var item = obj[i];
-  				if(typeof item === "string") { 
-  					item = '"' + item + '"'; 
-  				}
   				if(i === obj.length - 1){
   					temp += stringifyJSON(item);
   				} else {
@@ -32,15 +37,33 @@ var stringifyJSON = function(obj) {
   			}
   			temp += "]";	
   		} else {
-  			obj = "{";
-  			for (var itm in obj){
-
+  			//This is needed to find out what the last key is in order to not add a comma at the end.
+  			var lengthKeys = Object.keys(obj).length;
+  			var counter = 1;
+  			temp = "{";
+  			for (var key in obj){
+  				// all keys are strings.
+  				if (obj.hasOwnProperty(key)){
+  					var value = obj[key];
+	  				if(typeof value === 'function' || typeof value === 'undefined' ) { return "{}"; }
+	  				//if value is a string add extra quotes for them to appear during concatenation.
+	  				if (counter === lengthKeys){
+	  					temp += stringifyJSON(key) + ":" + stringifyJSON(value);
+	  				} else {
+	  					temp += stringifyJSON(key) + ":" + stringifyJSON(value) + ",";
+	  				}
+	  				counter++;
+  				}
   			}
+  			temp += "}";
   		}
   		obj = temp;
   	break;
+  	case "function":
+  		return undefined;
+  		break;
   	default:
-  		obj = null;
+  		obj;
   }
   return obj;
 };
